162b2cf9bb75dc91acc4d72afb26121a44628b09
vrkeel <vladkeel@gmail.com>
2020-05-06 06:00:30 +0300

    manipulators as classes

--- manipulators.py
+++ manipulators.py
@@ -5,42 +5,63 @@ import numpy as np
 from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
 from sklearn import preprocessing
 
-NUMBER_OF_NEIGHBORS = 200
-
-map_of_knns = {}
+NUMBER_OF_NEIGHBORS = 20
 
 
-def fill_row(row, target, features, feature_map):
-    if not np.isnan(row[target]):
+class NAFiller:
+    def __init__(self):
+        self.map_of_knns = {}
+
+    def fit(self, data, feature_list, feature_map):
+        train_data = data.dropna()
+        for i, target in enumerate(feature_list):
+            origs = feature_list.copy()
+            origs.remove(target)
+            if isinstance(feature_map[target], CategoricalFeature):
+                self.map_of_knns[target] = KNeighborsClassifier(n_neighbors=NUMBER_OF_NEIGHBORS)
+            else:
+                self.map_of_knns[target] = KNeighborsRegressor(n_neighbors=NUMBER_OF_NEIGHBORS)
+            self.map_of_knns[target].fit(train_data[origs], train_data[target])
+
+    def _fill_row(self, row, target, features, feature_map):
+        if not np.isnan(row[target]):
+            return row
+        l_row = row.copy()
+        for feature in features:
+            if pd.isna(l_row[feature]):
+                l_row[feature] = feature_map[feature].replacement()
+        row[target] = self.map_of_knns[target].predict([l_row[features]])
         return row
-    l_row = row.copy()
-    for feature in features:
-        if pd.isna(l_row[feature]):
-            l_row[feature] = feature_map[feature].replacement()
-    row[target] = map_of_knns[target].predict([l_row[features]])
-    return row
+
+    def transform(self, data, feature_list, feature_map):
+        for i, target in enumerate(feature_list):
+            print(f'{i},', end='')
+            origs = feature_list.copy()
+            origs.remove(target)
+            data = data.apply(self._fill_row, axis=1, target=target, features=origs, feature_map=feature_map)
+        return data
 
 
-def fill_empty(data, feature_list, feature_map):
-    train_data = data.dropna()
-    print('Filling empty values')
-    num = len(feature_list)
+class DataNormalizer:
+    def __init__(self):
+        self.scalers = {}
 
-    for i, target in enumerate(feature_list):
-        origs = feature_list.copy()
-        origs.remove(target)
-        if isinstance(feature_map[target], CategoricalFeature):
-            map_of_knns[target] = KNeighborsClassifier(n_neighbors=NUMBER_OF_NEIGHBORS)
-        else:
-            map_of_knns[target] = KNeighborsRegressor(n_neighbors=NUMBER_OF_NEIGHBORS)
-        map_of_knns[target].fit(train_data[origs], train_data[target])
+    def fit(self, data, features, feature_map):
+        for feature in features:
+            if isinstance(feature_map[feature], CategoricalFeature):
+                continue
+            x = data[[feature]].values.astype(float)
+            self.scalers[feature] = preprocessing.MinMaxScaler()
+            self.scalers[feature].fit(x)
 
-    for i, target in enumerate(feature_list):
-        print(f'{i},', end='')
-        origs = feature_list.copy()
-        origs.remove(target)
-        data = data.apply(fill_row, axis=1, target=target, features=origs, feature_map=feature_map)
-    return data
+    def transform(self, data, feature, feature_map):
+        for feature in feature:
+            if isinstance(feature_map[feature], CategoricalFeature):
+                continue
+            x = data[[feature]].values.astype(float)
+            x_scaled = self.scalers[feature].transform(x)
+            data[feature] = pd.DataFrame(x_scaled, columns=[feature], index=data.index)
+        return data
 
 
 def clip_outliers(data, features, feature_map):
@@ -56,25 +77,25 @@ def enumerate_attrs(data):
     return data
 
 
-def normalize_features(data, features, feature_map):
-    for feature in features:
-        if isinstance(feature_map[feature], CategoricalFeature):
-            continue
-        #data[feature] = data[feature] - data[feature].
-        min_max_scaler = preprocessing.MinMaxScaler()
-        x = data[[feature]].values.astype(float)
-        x_scaled = min_max_scaler.fit_transform(x)
-        data[feature] = pd.DataFrame(x_scaled, columns=[feature], index=data.index)
-    return data
+class OneHot:
+    def __init__(self):
+        self.encoders = {}
+
+    def fit(self, data, features, feature_map):
+        for feature in features:
+            if isinstance(feature_map[feature], CategoricalFeature):
+                self.encoders[feature] = preprocessing.OneHotEncoder(handle_unknown='ignore')
+                self.encoders[feature].fit(data[feature])
+                feature_map[feature].sub_features = self.encoders[feature].get_feature_names(['feature'])
+
+    def transform(self, data, features, feature_map):
+        for feature in features:
+            if isinstance(feature_map[feature], CategoricalFeature):
+                new_features = self.encoders[feature].transform(data[feature])
+                new_names = feature_map[feature].sub_features
+                for i, sub_feat in enumerate(new_names):
+                    data[sub_feat] = pd.DataFrame(new_features[:, i], columns=[sub_feat], index=data.index)
+                data = data.drop(feature, axis=1)
+        return data
 
 
-def one_hot(data, features, feature_map):
-    for feature in features:
-        if isinstance(feature_map[feature], CategoricalFeature):
-            new_features = pd.get_dummies(data[feature], prefix=feature)
-            new_names = new_features.columns
-            feature_map[feature].sub_features = new_names
-            for sub_feat in new_names:
-                data[sub_feat] = new_features[sub_feat]
-            data = data.drop(feature, axis=1)
-    return data
--- part2.py
+++ part2.py
@@ -11,26 +11,43 @@ def main():
     raw_data = load_data('ElectionsData.csv')
     data = raw_data.copy()
     #raw data saved aside for future inspection
-    data = manipulators.enumerate_attrs(data)
-    features_map = map_features(data)
-    features = list(features_map.keys())
-    data = manipulators.fill_empty(data, features, features_map)
-    data = manipulators.clip_outliers(data, features, features_map)
-    data = manipulators.normalize_features(data, features, features_map)
-    data, features, features_map = featureSelection.mutal_information_filter(data, features, features_map)
-    data = manipulators.one_hot(data, features, features_map)
-    #sfs with knn
-    sfs_knn_features = featureSelection.sfs(data, features, features_map, KNeighborsClassifier(n_neighbors=200), 'sfs_knn.csv')
-    #sfs with svm
-    sfs_svm_features = featureSelection.sfs(data, features, features_map, SVC(gamma='auto'), 'sfs_svm.csv')
-    #featureSelection.iterative_k_best(data, RandomForestClassifier(n_estimators=100))
-
     raw_train, raw_val, raw_test = split_data(data)
     train, val, test = raw_train.copy(), raw_val.copy(), raw_test.copy()
     train_x, train_y = data_get_label(train)
     val_x, val_y = data_get_label(val)
     test_x, test_y = data_get_label(test)
-    print(train_x)
+
+    train_x = manipulators.enumerate_attrs(train_x)
+    val_x = manipulators.enumerate_attrs(val_x)
+    test_x = manipulators.enumerate_attrs(test_x)
+    features_map = map_features(train_x)
+    features = list(features_map.keys())
+
+    filler = manipulators.NAFiller()
+    filler.fit(data, features, features_map)
+    normalizer = manipulators.DataNormalizer()
+    normalizer.fit(data, features, features_map)
+    one_hot = manipulators.OneHot()
+    one_hot.fit(data, features, features_map)
+
+    train_x = filler.transform(train_x, features, features_map)
+    val_x = filler.transform(val_x, features, features_map)
+    test_x = filler.transform(test_x, features, features_map)
+
+    train_x = normalizer.transform(train_x, features, features_map)
+    val_x = normalizer.transform(val_x, features, features_map)
+    test_x = normalizer.transform(test_x, features, features_map)
+
+    train_x = one_hot.transform(train_x, features, features_map)
+    val_x = one_hot.transform(val_x, features, features_map)
+    test_x = one_hot.transform(test_x, features, features_map)
+
+    #sfs with knn
+    sfs_knn_features = featureSelection.sfs(data, features, features_map, KNeighborsClassifier(n_neighbors=5), 'sfs_knn.csv')
+    #sfs with svm
+    sfs_svm_features = featureSelection.sfs(data, features, features_map, RandomForestClassifier(n_estimators=100), 'sfs_forest.csv')
+    #featureSelection.iterative_k_best(data, RandomForestClassifier(n_estimators=100))
+
     a = 0
 
 
